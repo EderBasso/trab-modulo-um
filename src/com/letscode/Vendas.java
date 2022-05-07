1c179f0493ac18a2340b09f5e3b6c34a1eaed90a
Cinthia Yoshie <901996+CinthiaY@users.noreply.github.com>
2022-05-06 21:29:21 -0300

    quase pronto!

--- src/com/letscode/Vendas.java
+++ src/com/letscode/Vendas.java
@@ -7,8 +7,13 @@ public class Vendas {
     public static Object [][] matrizTeste = {{1, "Marca", "tipo", "nome", 1.5, 4.5, 0, 100, "data"},
             {2, "Marca2", "tipo2", "nome2", 10.5, 20.5, 2, 200, "data2"}}; //matriz só para testar esta classe
 
+    public static Object[][] matrizVendas = new Object[1][4];
+    public static int indice =0;
+    public static String [] matrizCpf = new  String[1];
+    public static int indiceCpf =0;
 
     public static void main(String[] args) {
+
         vender(matrizTeste);
     }
 
@@ -57,25 +62,82 @@ public class Vendas {
             String nome = (String)produtos[i][3];
             double precoUnit = (Double)produtos[i][5];
             int estoqueAtual = (Integer)produtos[i][6];
-//            System.out.println("i" +i +"\n nome" +nome +"\n preco" +precoUnit +"\n estoque"+ estoqueAtual);
-            double preco = quantidade*precoUnit;
+            if(quantidade>estoqueAtual){
+                System.out.println("Estoque insuficiente. Estoque atual:" + estoqueAtual);
+            }
+            else {
+                //            System.out.println("i" +i +"\n nome" +nome +"\n preco" +precoUnit +"\n estoque"+ estoqueAtual);
+                double preco = quantidade * precoUnit;
+                precoTotal += preco;
+                quantidadeTotal += quantidade;
+                Main.produtos[i][6] = (Integer)Main.produtos[i][6]-quantidade;
 
-            precoTotal += preco;
-
-            quantidadeTotal += quantidade;
-
-            String notaFiscalNova = notaFiscal + "\n"+identificador+" | "+nome+" | "+quantidade+" | "+precoUnit+" | "+preco;
-            System.out.println(notaFiscalNova);
-            notaFiscal = notaFiscalNova;
+                String notaFiscalNova = notaFiscal + "\n" + identificador + " | " + nome + " | " + quantidade + " | " + precoUnit + " | " + preco;
+                System.out.println(notaFiscalNova);
+                notaFiscal = notaFiscalNova;
+            }
             }
         }
 
+        precoTotal = precoTotal*(1- tipoCliente.getDesconto());
+
         System.out.println("Quantidade de produtos "+quantidadeTotal+"\n"+"Valor total pago "+precoTotal);
-        System.out.println("CPF Cliente: "+cpf+"\n"+tipoCliente.getDescricao()+"\n ");
+        System.out.println("CPF Cliente: "+cpf+"\n"+tipoCliente.getDescricao()+"\n Desconto de: "+tipoCliente.getDesconto()*100+"% aplicado");
+        adicionarVendas(cpf, tipoCliente, quantidadeTotal, precoTotal);
+
+
+
 
     }
 
+    public static void adicionarVendas(String cpf, TipoCliente tipoCliente, Integer quantidadeTotal, Double precoTotal) {
+        if (indice == matrizVendas.length){
+            redimensionar();
+        }
+        matrizVendas[indice][0]=cpf;
+        matrizVendas[indice][1]= tipoCliente;
+        matrizVendas[indice][2]=quantidadeTotal;
+        matrizVendas[indice][3]=precoTotal;
+        indice++;
+        cadastroCpf(cpf);
 
+    }
+
+    public static void redimensionar(){
+        Object[][] newMatrix = new Object[matrizVendas.length*2][4];
+        for (int i =0; i< matrizVendas.length; i++){
+            newMatrix[i] = matrizVendas[i];
+        }
+        matrizVendas = newMatrix;
+    }
+
+    public static void redimensionarCpf(){
+        String[] newMatrix = new String[matrizCpf.length*2];
+        for (int i =0; i< matrizCpf.length; i++){
+            newMatrix[i] = matrizCpf[i];
+        }
+        matrizCpf = newMatrix;
+    }
+
+    public static void cadastroCpf(String cpf){
+        if (indiceCpf == matrizCpf.length){
+            redimensionarCpf();
+        }
+        boolean cpfExiste = false;
+
+        for(int i =0; i<matrizCpf.length; i++){
+            if (matrizCpf[i]!= null){
+                if(matrizCpf[i]==cpf){
+                    cpfExiste = true;
+                }
+            }
+        }
+        if (!cpfExiste){
+            matrizCpf[indiceCpf]=cpf;
+            indiceCpf++;
+        }
+
+    }
 
 
 }
